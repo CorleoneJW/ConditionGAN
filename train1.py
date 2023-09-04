03f209c2a1ae39051c58459eb6c8a510de256bd5
CorleoneJW <252653889@qq.com>
2023-09-04 17:16:45 +0800

    change the process of training

--- train1.py
+++ train1.py
@@ -32,34 +32,36 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
-    parser.add_argument('--batch_size', type=int, default=32 , help='size of the batches')
+    parser.add_argument('--batch_size', type=int, default=16 , help='size of the batches')
     parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
     parser.add_argument('--beta1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
     parser.add_argument('--beta2', type=float, default=0.999, help='adam: decay of second order momentum of gradient')
     parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
     parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
-    parser.add_argument('--n_classes', type=int, default=10, help='number of classes for dataset')
-    parser.add_argument('--img_size', type=int, default=28, help='size of each image dimension')
+    parser.add_argument('--n_classes', type=int, default=1, help='number of classes for dataset')
+    parser.add_argument('--img_size', type=int, default=256, help='size of each image dimension')
     parser.add_argument('--channels', type=int, default=1, help='number of image channels')
     parser.add_argument('--sample_interval', type=int, default=200, help='interval between image sampling')
     args = parser.parse_args()
     print(args)
 
     C,H,W = args.channels, args.img_size, args.img_size
+    img_size = args.img_size
+    latent_dim = args.latent_dim
 
     # Loss function
-    adversarial_loss = torch.nn.MSELoss()
+    criterion = torch.nn.MSELoss()
     # Initialize Generator and discriminator
-    generator = Generator(H,W)
-    discriminator = Discriminator(H,W)
+    generator = Generator(latent_dim,C,img_size)
+    discriminator = Discriminator(C,img_size)
 
     if torch.cuda.is_available():
         # generator.cuda()
         # discriminator.cuda()
-        # adversarial_loss.cuda()
+        # criterion.cuda()
         generator = generator.to(device)
         discriminator = discriminator.to(device)
-        adversarial_loss = adversarial_loss.to(device)
+        criterion = criterion.to(device)
 
     # Initialize weights
     generator.apply(weights_init_normal)
@@ -69,6 +71,7 @@ if __name__ == "__main__":
     transform = Compose([
         Resize((256, 256)),
         ToTensor()
+        # transforms.Normalize(mean=[0.5], std=[0.5])  # 标准化到[-1, 1]
     ])
 
     # prepare dataset
@@ -81,80 +84,104 @@ if __name__ == "__main__":
 
     train_dataset, val_dataset, test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
 
-    batch_size = 16
+    batch_size = args.batch_size
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
     # Optimizers
-    optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
-    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+    gen_optimizer = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+    disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
 
     #Start training
     batches_done=0
-    for epoch in range(args.n_epochs):
+    num_epochs = args.n_epochs
+    for epoch in range(num_epochs):
+        disc_loss_list = []
+        gene_loss_list = []
         for i, (imgs, labels) in enumerate(train_dataloader):
+            real_images = imgs
+            real_images = real_images.to(device)
+            labels = labels.to(device)
 
-            Batch_Size = args.batch_size
-            N_Class = args.n_classes
-            # Adversarial ground truths
-            valid = Variable(torch.ones(Batch_Size).cuda(), requires_grad=False)
-            fake = Variable(torch.zeros(Batch_Size).cuda(), requires_grad=False)
+            disc_optimizer.zero_grad()
 
-            # Configure input
-            real_imgs = Variable(imgs.type(torch.FloatTensor).cuda())
+            # 真实图像的标签为1
+            valid = torch.ones(real_images.size(0), 1).to(device)
 
-            real_y = torch.zeros(Batch_Size, N_Class)
-            real_y = Variable(real_y.scatter_(1, labels.view(Batch_Size, 1), 1).cuda())
-            #y = Variable(y.cuda())
+            # 生成器生成图像
+            z = torch.randn(real_images.size(0), latent_dim).to(device)
+            gen_images = generator(z)
 
-            # Sample noise and labels as generator input
-            noise = Variable(torch.randn((Batch_Size, args.latent_dim)).cuda())
-            gen_labels = (torch.rand(Batch_Size, 1) * N_Class).type(torch.LongTensor)
-            gen_y = torch.zeros(Batch_Size, N_Class)
-            gen_y = Variable(gen_y.scatter_(1, gen_labels.view(Batch_Size, 1), 1).cuda())
+            # 生成图像的标签为0
+            fake = torch.zeros(real_images.size(0), 1).to(device)
 
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
-            optimizer_D.zero_grad()
-            # Loss for real images
-            d_real_loss = adversarial_loss(discriminator(real_imgs, real_y).squeeze(), valid)
-            # Loss for fake images
-            gen_imgs = generator(noise, gen_y)
-            d_fake_loss = adversarial_loss(discriminator(gen_imgs.detach(),gen_y).squeeze(), fake)
-            # Total discriminator loss
-            d_loss = (d_real_loss + d_fake_loss)
+            # 判别器对真实图像的损失
+            real_loss = criterion(discriminator(real_images), valid)
+            # 判别器对生成图像的损失
+            fake_loss = criterion(discriminator(gen_images.detach()), fake)
+            # 判别器总损失
+            disc_loss = 0.5 * (real_loss + fake_loss)
 
-            d_loss.backward()
-            optimizer_D.step()
+            disc_loss.backward()
+            disc_optimizer.step()
 
-            # -----------------
-            #  Train Generator
-            # -----------------
+             # 训练生成器
+            gen_optimizer.zero_grad()
 
-            optimizer_G.zero_grad()
+            # 生成器生成图像并将其输入判别器
+            gen_images = generator(z)
+            gen_loss = criterion(discriminator(gen_images), valid)
 
-            # Generate a batch of images
-            #gen_imgs = generator(noise, gen_y)
-            # Loss measures generator's ability to fool the discriminator
-            g_loss = adversarial_loss(discriminator(gen_imgs,gen_y).squeeze(), valid)
+            gen_loss.backward()
+            gen_optimizer.step()
 
-            g_loss.backward()
-            optimizer_G.step()
+            disc_loss_list.append(disc_loss)
+            gene_loss_list.append(gen_loss)
 
+            # 将数据展平为 [batch_size, 1*256*256]
+            # real_images = real_images.view(-1, 1 * img_size * img_size)     # torch.Size([batch_size, 65536])
 
-            print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, args.n_epochs, i, len(train_dataloader),
-                                                                d_loss.data.cpu(), g_loss.data.cpu()))
+            # 创建条件向量 [batch_size, num_classes]，并将其与输入连接
+            # label_one_hot = torch.zeros(batch_size, C)
+            # label_one_hot[range(batch_size), labels] = 1.0
+            # label_one_hot = label_one_hot.float()
+            # input_data = torch.cat([real_images, label_one_hot], dim=1)
 
-            batches_done = epoch * len(train_dataloader) + i
-            if batches_done % args.sample_interval == 0:
-                noise = Variable(torch.FloatTensor(np.random.normal(0, 1, (N_Class**2, args.latent_dim))).cuda())
-                #fixed labels
-                y_ = torch.LongTensor(np.array([num for num in range(N_Class)])).view(N_Class,1).expand(-1,N_Class).contiguous()
-                y_fixed = torch.zeros(N_Class**2, N_Class)
-                y_fixed = Variable(y_fixed.scatter_(1,y_.view(N_Class**2,1),1).cuda())
+            # 训练判别器
+            # disc_optimizer.zero_grad()
+            # real_labels = torch.ones(batch_size, 1)
+            # fake_labels = torch.zeros(batch_size, 1)
 
-                gen_imgs = generator(noise, y_fixed).view(-1,C,H,W)
+            # 使用真实图像计算判别器损失
+            # real_outputs = discriminator(input_data)
+            # real_loss = criterion(real_outputs, real_labels)
+            # real_loss.backward()
 
-                save_image(gen_imgs.data, img_save_path + '/%d-%d.png' % (epoch,batches_done), nrow=N_Class, normalize=True)
+            # 使用生成的图像计算判别器损失
+            # z = torch.randn(batch_size, latent_dim)
+            # fake_images = generator(torch.cat([z, label_one_hot], dim=1))
+            # fake_inputs = torch.cat([fake_images, label_one_hot], dim=1)
+            # fake_outputs = discriminator(fake_inputs.detach())
+            # fake_loss = criterion(fake_outputs, fake_labels)
+            # fake_loss.backward()
+
+            # 更新判别器权重
+            # disc_optimizer.step()
+            # total_disc_loss = real_loss + fake_loss
+
+            # 训练生成器
+            # gen_optimizer.zero_grad()
+            # fake_outputs = discriminator(torch.cat([fake_images, label_one_hot], dim=1))
+            # gen_loss = criterion(fake_outputs, real_labels)
+            # gen_loss.backward()
+
+            # 更新生成器权重
+            # gen_optimizer.step()
+
+        # 打印训练信息
+        print(f"Epoch [{epoch + 1}/{num_epochs}] | d_loss: {disc_loss_list.mean():.4f} | g_loss: {gene_loss_list.mean():.4f}")
+
+    # 保存生成器模型
+    torch.save(generator.state_dict(), 'generator.pth')
+    torch.save(discriminator.state_dict(), 'discriminator.pth')
